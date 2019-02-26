d6ac7d53f808e05a16285cb652ff8a39040aaa76
Shawn <ssaffarshargh@appacademy.io>
2019-02-25 17:42:30 -0800

    Sample app source

---
+++ app/app.js
@@ -0,0 +1,14 @@
+const Redis = require('ioredis');
+// `redis` is registered in /etc/hosts
+// automatically by docker-compose
+// it maps to the ip address of the
+// redis container and is akin to
+// localhost -> 127.0.0.1
+const redis = new Redis(6379, 'redis');
+
+(async () => {
+  await redis.set('foo', 'bar');
+  const results = await redis.get('foo');
+  
+  console.log({ results });
+})();
